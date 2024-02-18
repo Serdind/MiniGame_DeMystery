288e68797b7cfee5ca84d09e85b0c7bab6944c91
Serdind <1055241@hr.nl>
2024-02-18 19:59:41 +0100

    Location CurrentLocation

--- MiniProject/Player.cs
+++ MiniProject/Player.cs
@@ -1,12 +1,12 @@
 public class Player
 {
     public string Name;
-    public string CurrentLocation;
+    public Location CurrentLocation;
     public string CurrentWeapon;
     public int CurrentHitPoints;
     public int MaximumHitPoints;
 
-    public Player(string name, string currentLocation, string currentWeapon, int currentHitPoints, int maximumHitPoints)
+    public Player(string name, Location currentLocation, string currentWeapon, int currentHitPoints, int maximumHitPoints)
     {
         Name = name;
         CurrentLocation = currentLocation;
